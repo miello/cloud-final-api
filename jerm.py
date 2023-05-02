68dfbd05839283f6fcf36bd2774dfe0eb136f976
samithiwat <samithiwat2544@gmail.com>
2023-05-03 02:33:51 +0700

    feat: create jerm service

---
+++ jerm.py
@@ -0,0 +1,21 @@
+from PyPDF3 import PdfFileMerger, PdfFileWriter, PdfFileReader
+import random
+
+merger = PdfFileMerger()
+
+base_path = "./assets/watermark"
+pdf_file = "doc.pdf"
+watermark = ["watermark1.pdf","watermark2.pdf"]
+merged_file = "merged.pdf"
+
+def jerm(pdf_file):
+    input_pdf = PdfFileReader(open(pdf_file,'rb'))
+    watermark_pdf = PdfFileReader(open(base_path+random.choice(watermark), "rb"))
+
+    pdf_page = input_pdf.getPage(0)
+    pdf_page.mergePage(watermark_pdf.getPage(0))
+
+    output = PdfFileWriter()
+    output.addPage(pdf_page)
+
+    return open(merged_file,'wb')
